c25c0f4e86a688fbc200f40fa8493fc41cc801bf
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-22 20:46:04 +0900

    update

--- 20200113.php
+++ 20200113.php
@@ -1,52 +1,52 @@
 <?php
-// header("Content-Type: application/json; charset=UTF-8"); 
-// $array=[
-//   "id"=>1,
-//   "username"=>"bombman",
-//   "gender"=>"male",
-//   "married"=>false
-// ];
-$string = 'male';
-// $json_encode = json_encode($array);
-$json_encode = json_encode($string);
-setcookie('php_to_js', $json_encode,time()+86400,'/');
-// echo $_COOKIE['php_to_js'].'<br>';
-// $_COOKIE['php_to_js'] ='take this';
+// // header("Content-Type: application/json; charset=UTF-8"); 
+// // $array=[
+// //   "id"=>1,
+// //   "username"=>"bombman",
+// //   "gender"=>"male",
+// //   "married"=>false
+// // ];
+// $string = 'male';
+// // $json_encode = json_encode($array);
+// $json_encode = json_encode($string);
+// setcookie('php_to_js', $json_encode,time()+86400,'/');
+// // echo $_COOKIE['php_to_js'].'<br>';
+// // $_COOKIE['php_to_js'] ='take this';
 
-// /**js json send to php */
-// header('Content-type: application/json');
-header('content-type','application/x-www-form-urlencoded');
-// echo filter_input_array(INPUT_POST);
-echo file_get_contents('php://input');
-$data = json_decode( file_get_contents( 'php://input' ), true );
-$str_json = file_get_contents('php://input');
-// var_dump($str_json);
-// var_dump($data);
-// var_dump($HTTP_RAW_POST_DATA);
+// // /**js json send to php */
+// // header('Content-type: application/json');
+// header('content-type','application/x-www-form-urlencoded');
+// // echo filter_input_array(INPUT_POST);
+// echo file_get_contents('php://input');
+// $data = json_decode( file_get_contents( 'php://input' ), true );
+// $str_json = file_get_contents('php://input');
+// // var_dump($str_json);
+// // var_dump($data);
+// // var_dump($HTTP_RAW_POST_DATA);
 
-if($_SERVER["REQUEST_METHOD"] == "POST" && $_SERVER["CONTENT_TYPE"] == "application/json")
-{
-  $data = file_get_contents("php://input", false, stream_context_get_default(), 0, $_SERVER["CONTENT_LENGTH"]);
-  global $_POST_JSON;
-  $_POST_JSON = json_decode($_REQUEST["JSON_RAW"],true);
-  // var_dump($_POST_JSON);
-  // merge JSON-Content to $_REQUEST 
-  if(is_array($_POST_JSON)) $_REQUEST   = $_POST_JSON+$_REQUEST;
-}
-// var_dump($_REQUEST);
-// echo ' js json<br>';
-
-if($_COOKIE!=null){
-  // var_dump($_COOKIE);
-  // echo ' cookie<br>';
-}
-// require_once('js_to_php_with_ajax.php');
-// if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest'){
-//   $ajax = 'ajax has been sent.<br>';
-// }else{
-//   $ajax = 'no ajax here<br>';
+// if($_SERVER["REQUEST_METHOD"] == "POST" && $_SERVER["CONTENT_TYPE"] == "application/json")
+// {
+//   $data = file_get_contents("php://input", false, stream_context_get_default(), 0, $_SERVER["CONTENT_LENGTH"]);
+//   global $_POST_JSON;
+//   $_POST_JSON = json_decode($_REQUEST["JSON_RAW"],true);
+//   // var_dump($_POST_JSON);
+//   // merge JSON-Content to $_REQUEST 
+//   if(is_array($_POST_JSON)) $_REQUEST   = $_POST_JSON+$_REQUEST;
 // }
-// echo $ajax;
+// // var_dump($_REQUEST);
+// // echo ' js json<br>';
+
+// if($_COOKIE!=null){
+//   // var_dump($_COOKIE);
+//   // echo ' cookie<br>';
+// }
+// // require_once('js_to_php_with_ajax.php');
+// // if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest'){
+// //   $ajax = 'ajax has been sent.<br>';
+// // }else{
+// //   $ajax = 'no ajax here<br>';
+// // }
+// // echo $ajax;
 ?>
 
 
