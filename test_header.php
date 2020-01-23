a560abe83e77c6f7964ebd35223b72b6ad8ad3d4
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-23 11:49:04 +0900

    update

--- test_header.php
+++ test_header.php
@@ -2,46 +2,50 @@
 # anynoumous function 無名自我執行function 
 #(function([$para_alias]){code})()
 (function(){ 
+  $domain =$_SERVER['HTTP_HOST'];
   global $redirect;
   if(!isset($redirect)){$redirect=null;}
   global $wait_time;
   if(!isset($wait_time)){$wait_time=10;}
-  switch($redirect){
-    case 'root_directory':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#");
-      exit();
-      break;
-    case 'html_userdetail_template.php':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#/html_userdetail_template.php");
-      exit();
-      break;
-    case 'html_databasemanagement_template.php':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#/html_databasemanagement_template.php");
-      exit();
-      break;
-    case 'https://bombmanbombman-project1.herokuapp.com/#':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/");
-      exit();
-      break;
-    case 'html_searchproduct_template.php':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#/html_searchproduct.php");
-      exit();
-      break;
-    case 'html_userregister_template.php':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#/html_userregister_template.php");
-      exit();
-      break;
-    case 'html_showallproduct_template.php':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#/html_showallproduct_template.php");
-      exit();
-      break;
-    case 'html_marketimage_template.php':
-      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#/html_marketimage_template.php");
-      exit();
-      break;
-    default:
-    header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/#");
-  }
+  // if(strpos($domain,'herokuapp.com')){
+  echo "<div>".strpos($domain,'herokuapp.com')."</div>";
+    switch($redirect){
+      case 'root_directory':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com");
+        exit();
+        break;
+      case 'html_userdetail_template.php':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/html_userdetail_template.php");
+        exit();
+        break;
+      case 'html_databasemanagement_template.php':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/html_databasemanagement_template.php");
+        exit();
+        break;
+      case 'https://bombmanbombman-project1.herokuapp.com/#':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/");
+        exit();
+        break;
+      case 'html_searchproduct_template.php':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/html_searchproduct.php");
+        exit();
+        break;
+      case 'html_userregister_template.php':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/html_userregister_template.php");
+        exit();
+        break;
+      case 'html_showallproduct_template.php':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/html_showallproduct_template.php");
+        exit();
+        break;
+      case 'html_marketimage_template.php':
+        header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com/html_marketimage_template.php");
+        exit();
+        break;
+      default:
+      header("refresh:$wait_time;url=https://bombmanbombman-project1.herokuapp.com");
+    }
+  // }
 })();
 exit();
 ?>
