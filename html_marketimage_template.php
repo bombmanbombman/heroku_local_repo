771f65a917f78dee5b4d6d45b2249e1a9027b59c
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-28 00:00:34 +0900

    upload

--- html_marketimage_template.php
+++ html_marketimage_template.php
@@ -40,7 +40,7 @@ if(isset($_SESSION)){
 }
 require_once("html_navibar_template.php");
 if(!isset($_SESSION['user_id'])){
-  echo "session 傳送失敗<br>";
+  echo "<div id='echo1'>session 傳送失敗</div>";
   $redirect='html_login_template.php';
   require_once ('test_header.php');
   exit();
@@ -83,12 +83,12 @@ foreach($rows as $subarray1){
 }
 // var_dump($COLID);
 if(!in_array($_SESSION['product_id_for_image'],$COLID)){
-  echo"這個貨號不存在，請輸入有效的貨號<br>";
+  echo"<div id='echo2'>這個貨號不存在，請輸入有效的貨號</div>";
   $redirect='html_showallproduct_template.php';
   require_once ("test_header.php");
 }
 if($conn->connect_error){
-  echo "無法連接數據庫，$databasename ， 回到login畫面","<br>";
+  echo "<div><span id='echo3'>無法連接數據庫，</span>$databasename <span id='echo4'>， 回到login畫面</span></div>";
   require_once ('test_header.php');
   exit();
 }
@@ -100,15 +100,16 @@ if(!$stmt)echo($conn->error);
 $product_id=$_SESSION['product_id_for_image'];
 $stmt->bind_param('ii',$user_id,$product_id);
 if(!$stmt->execute()){
-  echo "尋找對應用戶號失敗， 回到选择頁面","<br>";
-  header("refresh:2;url=http://localhost:8012/laravelFolder/resources/views/leanewentry.php");
+  echo "<div id='echo5'>尋找對應用戶號失敗， 回到选择頁面</div>";
+  $redirect='html_showallproduct_template.php';
+  require_once('test_header.php');
   exit();
 }
 $result_stmt=$stmt->get_result();
 while($row=$result_stmt->fetch_assoc()){
   echo "
     
-    <table><tr><th>貨號  </th><th>進貨地點</th><th>貨品簡介</th><th>貨品詳細</th></tr>
+    <table><tr><th><span id='echo6'>貨號</span></th><th><span id='echo7'>進貨地點</span></th><th><span id='echo8'>貨品簡介</span></th><th><span id='echo9'>貨品詳細</span></th></tr>
     <tr>
     <th>$row[product_id]</th>
     <th>$row[buy_place]</th>
@@ -121,10 +122,10 @@ while($row=$result_stmt->fetch_assoc()){
 echo "<form id='form_for_delete' action ='html_deleteimage_template.php' method = 'post'>";
 for($i=0;$i<$image_number_of_this_product;$i++){
   echo "<table class='float_left'>";
-  echo " <tr ><th>圖片編號 $all_image_id_of_this_product[$i]</th></tr>";
+  echo " <tr ><th><span id='echo10'>圖片編號</span> $all_image_id_of_this_product[$i]</th></tr>";
   echo "<td><img height='80' width='80 'src='data:image/jpeg;base64,"
   .base64_encode($all_image_data_of_this_product[$i])."'/></td></<td>";
-  echo " <tr ><td><input type='submit' name='$all_image_id_of_this_product[$i]' value='刪除圖片'></td></tr>";
+  echo " <tr ><td><input type='submit' id='value1' name='$all_image_id_of_this_product[$i]' value='刪除圖片'></td></tr>";
 
 echo "</table>";
 }
@@ -133,20 +134,20 @@ echo "<span class='clear_float'></span>";
 echo "  <br><br><br><br><br><br><br>";
 
 
-echo "最左邊的圖片會作為縮略圖在 市場中顯示。";
+echo "<div id='echo11'>最左邊的圖片會作為縮略圖在 市場中顯示。</div>";
 if($image_number_of_this_product>=6){
-  echo "<h3>已經到達上傳圖片的數量限制，請刪除一些圖片</h3>";
+  echo "<h3 id='echo12'>已經到達上傳圖片的數量限制，請刪除一些圖片</h3>";
 }elseif($image_number_of_this_product<6){
-  echo "<h4>您已經為這個貨號上傳了 $image_number_of_this_product 張圖片，請不要超過6張。</h4>";
+  echo "<h4><span id='echo13'>您已經為這個貨號上傳了</span> $image_number_of_this_product <span id='echo14'>張圖片，請不要超過6張。</span></h4>";
   echo <<<_HEREDOC
   <form method="post" action="html_submitredirect_template.php" enctype='multipart/form-data'>
-  <label>請選擇上傳圖片，格式為jpg或png，每張20mb以下，解析度小於3000*3000</label><br>
+  <label id='echo15'>請選擇上傳圖片，格式為jpg或png，每張20mb以下，解析度小於3000*3000</label><br>
   <input type="hidden" name="date_image"  value='' >
   <input type="hidden" name='product_id_for_image' value=$product_id>
   <input type='file' name='image_data' required><br>
-  <label>照片描述（選填）在market上顯示</label><br>
+  <label id='echo16'>照片描述（選填）在market上顯示</label><br>
   <textarea name='image_info' maxlength='255' rows='4' cols='40' value=''></textarea><br>
-  <input type="submit" name='image_data' value="上傳這張圖片。">
+  <input type="submit" id='value2' name='image_data' value="上傳這張圖片。">
   </form>
 
 
