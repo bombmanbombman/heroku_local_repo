16fda3048f9d654657825a65e16b0d8672185397
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-28 18:01:24 +0900

    update

--- html_deleteimage_template.php
+++ html_deleteimage_template.php
@@ -6,33 +6,34 @@ if(!isset($_POST)){
   /**phpjs */
   echo 'error0';
 }
-if(isset($_POST)&&isset($_POST['image_id'])&&isset($_POST['product_id']))
-session_start();
-require_once('login.php');
-/**php*/
-// var_dump($_POST);
-// echo "<br>";
-// $check_image_id=array_keys($_POST);
-// $check_image_id=$check_image_id[0];
-// $user_id = $_SESSION['user_id'];
-// $query = 'delete from image 
-//             where image_id = '.$check_image_id.' and user_id ='.$user_id;
-// $stmt=$conn->query($query);
-// if(!$stmt)echo($conn->error);
-// echo "已經成功刪除了指定的圖片";
-// $redirect='html_marketimage_template.php';
-// require_once('test_header.php');
-
-/**phpjs*/
-
-$user_id = $_SESSION['user_id'];
-$query = 'delete from image 
-            where image_id = '.$_POST['image_id'].' and user_id ='.$user_id;
-$stmt=$conn->query($query);
-if(!$stmt){
-  echo($conn->error.'||error1');
-};
-echo "已經成功刪除了指定的圖片";
+if(isset($_POST)&&isset($_POST['image_id'])&&isset($_POST['product_id'])){
+  session_start();
+  require_once('login.php');
+  /**php*/
+  // var_dump($_POST);
+  // echo "<br>";
+  // $check_image_id=array_keys($_POST);
+  // $check_image_id=$check_image_id[0];
+  // $user_id = $_SESSION['user_id'];
+  // $query = 'delete from image 
+  //             where image_id = '.$check_image_id.' and   user_id ='.$user_id;
+  // $stmt=$conn->query($query);
+  // if(!$stmt)echo($conn->error);
+  // echo "已經成功刪除了指定的圖片";
+  // $redirect='html_marketimage_template.php';
+  // require_once('test_header.php');
+  
+  /**phpjs*/
+  
+  $user_id = $_SESSION['user_id'];
+  $query = 'delete from image 
+              where image_id = '.$_POST['image_id'].' and   user_id ='.$user_id;
+  $stmt=$conn->query($query);
+  if(!$stmt){
+    echo($conn->error.'||error1');
+  };
+  echo "已經成功刪除了指定的圖片";
+}
 
 
 
