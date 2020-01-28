dc5596e3864d024811eca91dc60b34aedf881746
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-29 02:39:02 +0900

    vue

--- html_newproduct_template.js
+++ html_newproduct_template.js
@@ -1,72 +1,60 @@
-$(function(){
-  const googleMap = new Vue({
-    el: '#app',
-    data: {
-      map: null,
-      autocomplete: null, // google map Autocomplete method
-      site: '', // place API要綁定的搜尋框
-      place: null // 存place確定後回傳的資料
+const googleMap = new Vue({
+  el: '#app',
+  data: {
+    map: null,
+    autocomplete: null,
+    site: '', // place API要綁定的搜尋框
+    place: null // 存place確定後回傳的資料
+  },
+  methods: {
+    // init google map
+    initMap() {
+
+      let location = {
+        lat: 25.0374865,
+        lng: 121.5647688
+      };
+
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: location,
+        zoom: 16
+      });
     },
-    methods: {
-      // init google map
-      initMap() {
-  
-        let location = {
-          lat: 25.0374865,
-          lng: 121.5647688
-        };
-  
-        this.map = new google.maps.Map(document.getElementById('map'), {
-          center: location,
-          zoom: 16
-        });
-        
-      },
-      // 地址自動完成 + 地圖的中心移到輸入結果的地址上
-      siteAuto() {
-  
-        let options = {
-          componentRestrictions: { country: 'tw' } // 限制在台灣範圍
-        };
-        this.autocomplete = new google.maps.places.Autocomplete(this.$refs.site, options);
-        
-        // 地址的輸入框，值有變動時執行
-        this.autocomplete.addListener('place_changed', () => {
-          this.place = this.autocomplete.getPlace(); // 地點資料存進place
+    // 地址自動完成 + 地圖的中心移到輸入結果的地址上
+    siteAuto() {
+
+      let options = {
+        componentRestrictions: { country: 'tw' } // 限制在台灣範圍
+      };
+      this.autocomplete = new google.maps.places.Autocomplete(this.$refs.site, options);
+      this.autocomplete.addListener('place_changed', () => {
+        this.place = this.autocomplete.getPlace();
+        if(this.place.geometry) {
+          let searchCenter = this.place.geometry.location;
+          this.map.panTo(searchCenter); // panTo是平滑移動、setCenter是直接改變地圖中心
           
-          // 確認回來的資料有經緯度
-          if(this.place.geometry) {
-            
-            // 改變map的中心點
-            let searchCenter = this.place.geometry.location;
-            
-            // panTo是平滑移動、setCenter是直接改變地圖中心
-            this.map.panTo(searchCenter);
-  
-            // 在搜尋結果的地點上放置標記
-            let marker = new google.maps.Marker({
-              position: searchCenter,
-              map: this.map
-            });
-  
-            // info window
-            let infowindow = new google.maps.InfoWindow({
-              content: this.place.formatted_address
-            });
-            infowindow.open(this.map, marker);
-  
-          }
-          
-        });
-      }
-    },
-    mounted() {
-      window.addEventListener('load', () => {
-  
-        this.initMap();
-        this.siteAuto();
-  
+          // 放置標記
+          let marker = new google.maps.Marker({
+            position: searchCenter,
+            map: this.map
+          });
+
+          // info window
+          let infowindow = new google.maps.InfoWindow({
+            content: this.place.formatted_address
+          });
+          infowindow.open(this.map, marker);
+
+        }
       });
     }
-  })
+  },
+  mounted() {
+    window.addEventListener('load', () => {
+
+      this.initMap();
+      this.siteAuto();
+
+    });
+  }
 });
--- html_newproduct_template.php
+++ html_newproduct_template.php
@@ -1,53 +1,52 @@
 <html>
   <head>
+    <title>new product ID</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta http-equiv="Content-Type" content="text/html;     charset=utf-8" />
-      <!-- map 必须要的css -->
-  <style>
-    /* Always set the map height explicitly to define the   size  of the div
-     * element that contains the map. */
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      .controls {
-        background-color: #fff;
-        border-radius: 2px;
-        border: 1px solid transparent;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-        box-sizing: border-box;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        height: 29px;
-        margin-left: 17px;
-        margin-top: 10px;
-        outline: none;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 400px;
-      }
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-      .controls:focus {
-        border-color: #4d90fe;
+      <!-- map 必须要的css -->
+      <style>
+      .container {
+        padding-top: 30px;
+        padding-bottom: 30px;
       }
-      .title {
-        font-weight: bold;
+      #map {
+        background: #CCC;
       }
-      #infowindow-content {
-        display: none;
+      ul {
+        padding-top: 16px;
       }
-      #map #infowindow-content {
-        display: inline;
+      .media img {
+        max-width: 64px;
       }
-  </style>
+      .media h5, p {
+        font-size: 14px;
+      }
+      .mdeia p {
+        margin-bottom: 6px;
+      }
+      .media h6 {
+        font-size: 12px;
+        color: #CCC;
+      }
+      .fixed-bottom {
+        position: fixed;
+        left: 16px;
+        bottom: 0;
+        max-width: 320px;
+      }
+    </style>
+    <link rel="canonical" href="https://letswrite.tw/google-map-api-place-api/">
+    <link rel="shortcut icon" href="https://i0.wp.com/letswrite.tw/wp-content/uploads/2019/07/cropped-letswrite512-1.jpg"/>
+    <!-- Google Tag Manager-->
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PGQ9WQT');
+    </script>
     <script id='jquery' src="jquery-3.4.1.js"></script>
     <!-- ripple effect library -->
     <script src="jquery.ripples.js"></script>
@@ -131,28 +130,60 @@ if(isset($_POST['buy_place']) && isset($_POST['product_info'])){
   <br>
   <!-- google map 显示的element -->
   <section id='google_map'>
-    <div id="app">
+  <div class="row fixed-bottom">
+      <div class="col">
+        <div class="alert alert-info" role="alert">
+          Let's Write 筆記文：<br/>
+          <a href="https://letswrite.tw/google-map-api-place-api/" target="_blank">Google Maps API學習筆記-4</a>
+        </div>
+      </div>
+    </div>
+
+    <div id="app" class="container">
+
       <!-- 搜尋框 -->
-      <div>
-        <input type="text" ref="site" v-model="site">
+      <div class="row">
+        <div class="col google-map">
+          <h5>Search：</h5>
+          <div class="form-group">
+            <input type="text" class="form-control" ref="site" v-model="site">
+          </div>
+        </div>
       </div>
+
       <!-- 放google map的div -->
-      <div>
-        <div id="map"></div>
+      <div class="row">
+        <div class="col google-map">
+            <h5>Google Map：</h5>
+          <div id="map" class="embed-responsive embed-responsive-16by9"></div>
+        </div>
       </div>
+
+      <hr>
+
       <!-- 放評論摘要的div -->
-      <div>
-        <ul>
-          <li>
-            <img :src="p.profile_photo_url">
-            <div>
-              <h5><a target="_blank" :href="p.author_url">{{ p.author_name }}</a></h5>
-              <p>{{ p.text }}</p>
-              <h6>{{ p.relative_time_description }}</h6>
+      <div class="row" v-if="place != null">
+        <div class="col" v-if="place.reviews != null">
+          <h5>評論：</h5>
+          <div class="row" v-for="p in place.reviews">
+            <div class="col">
+              <ul class="list-unstyled">
+                <li class="media">
+                  <img :src="p.profile_photo_url" class="mr-3">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">
+                      <a target="_blank" :href="p.author_url">{{ p.author_name }}</a>
+                    </h5>
+                    <p>{{ p.text }}</p>
+                    <h6>{{ p.relative_time_description }}</h6>
+                  </div>
+                </li>
+              </ul>
             </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
+
     </div>
   </section>
   <form method='post' action='<?php echo $_SERVER["PHP_SELF"];?>'>
@@ -174,7 +205,7 @@ if(isset($_POST['buy_place']) && isset($_POST['product_info'])){
   <script id='ref' defer async type='text/javascript' src='html_template.js'></script>
   <script id='js' defer async type=text/javascript src='html_newproduct_template.js'></script>
   <section id='google_map_js'>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5lki3Wn7GU8gZllmCyWc9VgkVDrH-_OA&libraries=places&callback=initMap" async defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5lki3Wn7GU8gZllmCyWc9VgkVDrH-_OA&libraries=places" async defer></script>
   </section>
   </body>
 </html>
