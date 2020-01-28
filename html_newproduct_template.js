bc610e30211b4565af09d814578a257b2764f9ec
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-29 03:38:39 +0900

    Update html_newproduct_template.js

--- html_newproduct_template.js
+++ html_newproduct_template.js
@@ -1,71 +1,73 @@
-const googleMap = new Vue({
-  el: '#app',
-  data: {
-    map: null,
-    autocomplete: null,
-    site: '', // place API要綁定的搜尋框
-    place: null // 存place確定後回傳的資料
-  },
-  methods: {
-    // init google map
-    initMap() {
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-        lat=position.coords.latitude;
-        lng=position.coords.longitude;
-        });
-      }else{
-        lat =35.059823;
-        lng =135.749073;
-      }
-      // let location = {
-      //   lat: lat,
-      //   lng: lng
-      // };
-
-      this.map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-          lat: lat,
-          lng: lng
-        },
-        zoom: 16
-      });
+$(function(){
+  const googleMap = new Vue({
+    el: '#app',
+    data: {
+      map: null,
+      autocomplete: null,
+      site: '', // place API要綁定的搜尋框
+      place: null // 存place確定後回傳的資料
     },
-    // 地址自動完成 + 地圖的中心移到輸入結果的地址上
-    siteAuto() {
-
-      let options = {
-        componentRestrictions: { country: 'jp' } // 限制在日本範圍
-      };
-      this.autocomplete = new google.maps.places.Autocomplete(this.$refs.site, options);
-      this.autocomplete.addListener('place_changed', () => {
-        this.place = this.autocomplete.getPlace();
-        if(this.place.geometry) {
-          let searchCenter = this.place.geometry.location;
-          this.map.panTo(searchCenter); // panTo是平滑移動、setCenter是直接改變地圖中心
-          
-          // 放置標記
-          let marker = new google.maps.Marker({
-            position: searchCenter,
-            map: this.map
+    methods: {
+      // init google map
+      initMap() {
+        if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(function (position){
+          lat=position.coords.latitude;
+          lng=position.coords.longitude;
           });
-
-          // info window
-          let infowindow = new google.maps.InfoWindow({
-            content: this.place.formatted_address
-          });
-          infowindow.open(this.map, marker);
-
+        }else{
+          lat =35.059823;
+          lng =135.749073;
         }
+        // let location = {
+        //   lat: lat,
+        //   lng: lng
+        // };
+      
+        this.map = new google.maps.Map(document.getElementById  ('map'), {
+          center: {
+            lat: lat,
+            lng: lng
+          },
+          zoom: 16
+        });
+      },
+      // 地址自動完成 + 地圖的中心移到輸入結果的地址上
+      siteAuto() {
+      
+        let options = {
+          componentRestrictions: { country: 'jp' } // 限制在日  本範圍
+        };
+        this.autocomplete = new google.maps.places.Autocomplete (this.$refs.site, options);
+        this.autocomplete.addListener('place_changed', () => {
+          this.place = this.autocomplete.getPlace();
+          if(this.place.geometry) {
+            let searchCenter = this.place.geometry.location;
+            this.map.panTo(searchCenter); // panTo是平滑移動、  setCenter是直接改變地圖中心
+            
+            // 放置標記
+            let marker = new google.maps.Marker({
+              position: searchCenter,
+              map: this.map
+            });
+          
+            // info window
+            let infowindow = new google.maps.InfoWindow({
+              content: this.place.formatted_address
+            });
+            infowindow.open(this.map, marker);
+          
+          }
+        });
+      }
+    },
+    mounted() {
+      window.addEventListener('load', () => {
+      
+        this.initMap();
+        this.siteAuto();
+      
       });
     }
-  },
-  mounted() {
-    window.addEventListener('load', () => {
-
-      this.initMap();
-      this.siteAuto();
-
-    });
-  }
-});
+  });
+})
