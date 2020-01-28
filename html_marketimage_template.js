543bfc078e8a5db1143f15403b8f633ef719835f
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-28 16:23:56 +0900

    update

--- html_marketimage_template.js
+++ html_marketimage_template.js
@@ -2,6 +2,20 @@ $(function(){
   $('.delete').on('click',function(e){
     e.preventDefault();
     console.log($('.active').attr('id'));
-    console.log(confirm());
+    if($('.active').attr('id')=='japanese'){
+      if(confirm('この写真を削除しますか？')){
+        console.log(e.target);
+      };
+    }
+    if($('.active').attr('id')=='chinese'){
+      if(confirm('是否确定删除这个图片？')){
+        console.log(e.target);
+      };
+    }
+    if($('.active').attr('id')=='english'){
+      if(confirm('would you like to delete this image？')){
+        console.log(e.target);
+      };
+    }
   })
 })
