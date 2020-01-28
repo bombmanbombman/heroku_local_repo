e7516a88f2a378c30cdf673617e34d6378238ab7
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-29 01:08:20 +0900

    update map

--- html_newproduct_template.js
+++ html_newproduct_template.js
@@ -1,6 +1,9 @@
 $(function(){
   var map;
+  var service;
+  var infowindow;
   function initMap() {
+    infowindow = new google.maps.Infowindow();
     map = new google.maps.Map(document.getElementById('map'), {
       // 京都市的location
       center: {lat: 35.116, lng: 135.768},
@@ -16,19 +19,19 @@ $(function(){
       map: map
     });
   };
-  $.ajax({
-    type:"GET",
-    dataType:'jsonp',
-    // 為了crossdomain request
-    crossDomain:'true',
-    url:"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyA1_8F9VSj4orU5N-_A-7Pb_y6BU0ajHso",
-  }).done(function(data){
-    console.log(JSON.parse(data));
-    // datastring=JSON.stringify(data);
-    // dataparse=JSON.parse(data);
-    // console.log(datastring);
-    // console.log(dataparse);
-  }).fail(function(error){
-    console.log(error);
-  })
+  // $.ajax({
+  //   type:"GET",
+  //   dataType:'jsonp',
+  //   // 為了crossdomain request
+  //   crossDomain:'true',
+  //   url:"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyA1_8F9VSj4orU5N-_A-7Pb_y6BU0ajHso",
+  // }).done(function(data){
+  //   console.log(JSON.parse(data));
+  //   // datastring=JSON.stringify(data);
+  //   // dataparse=JSON.parse(data);
+  //   // console.log(datastring);
+  //   // console.log(dataparse);
+  // }).fail(function(error){
+  //   console.log(error);
+  // })
 });
