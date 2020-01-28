a5f7795303eed01d6a7e03e055bbb499ac6ce7d7
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-29 03:42:00 +0900

    Update html_newproduct_template.js

--- html_newproduct_template.js
+++ html_newproduct_template.js
@@ -19,22 +19,19 @@ $(function(){
           lat =35.059823;
           lng =135.749073;
         }
-        // let location = {
-        //   lat: lat,
-        //   lng: lng
-        // };
-      
+        let location = {
+          lat: lat,
+          lng: lng
+        };
+
         this.map = new google.maps.Map(document.getElementById  ('map'), {
-          center: {
-            lat: lat,
-            lng: lng
-          },
+          center: location,
           zoom: 16
         });
       },
       // 地址自動完成 + 地圖的中心移到輸入結果的地址上
       siteAuto() {
-      
+
         let options = {
           componentRestrictions: { country: 'jp' } // 限制在日  本範圍
         };
@@ -44,29 +41,29 @@ $(function(){
           if(this.place.geometry) {
             let searchCenter = this.place.geometry.location;
             this.map.panTo(searchCenter); // panTo是平滑移動、  setCenter是直接改變地圖中心
-            
+
             // 放置標記
             let marker = new google.maps.Marker({
               position: searchCenter,
               map: this.map
             });
-          
+
             // info window
             let infowindow = new google.maps.InfoWindow({
               content: this.place.formatted_address
             });
             infowindow.open(this.map, marker);
-          
+
           }
         });
       }
     },
     mounted() {
       window.addEventListener('load', () => {
-      
+
         this.initMap();
         this.siteAuto();
-      
+
       });
     }
   });
