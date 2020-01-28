1ba9b1101d9e46a3b86f6517eef9a1c518e3c392
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-28 15:22:11 +0900

    update

--- html_template.js
+++ html_template.js
@@ -165,6 +165,7 @@ $(function(){
     }
   }
   //(2)時間表插入 
+  console.log($('a.active').attr('id'))
   if($('section#a_content').length||$('#time_table').length){
     $('#time_table').html(`
       <h4 id='clock' class='border border-warning rounded-pill rounded-lg bg-secondary text-center  text-white'></h4>
