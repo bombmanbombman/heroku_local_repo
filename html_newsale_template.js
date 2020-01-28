59f2b82c5582980528aa14d7a7ffc273bc611c0d
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-28 20:51:40 +0900

    Update html_newsale_template.js

---
+++ html_newsale_template.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  $('body').css({
+    "background-image": `url("/upload_compress/background_water4.jpg")`,
+    //device 的全屏 desktop 是整个显示器的解析度
+    "height": '130vh',
+    "width": "100%",
+    "position":"absolute",
+    "z-index":"-1",
+    "background-repeat":"no-repeat"
+  });
+})
