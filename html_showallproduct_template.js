6214e9864fa56a479a35b8c5fafff03cb4c87ccd
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-29 15:53:47 +0900

    update

--- html_showallproduct_template.js
+++ html_showallproduct_template.js
@@ -1,10 +1,10 @@
 $(function(){
-  $('#button1').on('click',function(e){
-    console.log(e.target);
-    window.location.href = 'html_newproduct_template.php';
-  })
-  $('#button2').on('click',function(e){
-    console.log(e.target);
-    window.location.href = 'html_displayimage_template.php';
-  })
+    $('#button1').on('click',function(e){
+        console.log(e.target);
+        window.location.href = 'html_newproduct_template.php';
+    })
+    $('#button2').on('click',function(e){
+        console.log(e.target);
+        window.location.href = 'html_displayimage_template.php';
+    })
 })
