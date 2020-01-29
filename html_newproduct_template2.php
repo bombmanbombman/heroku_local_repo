af7917edc2bd47a1a864b95d1a757167ab8dc591
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-01-29 21:35:29 +0900

    Update html_newproduct_template2.php

--- html_newproduct_template2.php
+++ html_newproduct_template2.php
@@ -1,46 +1,55 @@
 
 <div id="app" class="container">
-    <!-- 搜尋框 -->
-    <div class="row">
+      <!-- 搜尋框 -->
+      <div class="row">
         <div class="col google-map">
-            <h5>
-                Search：
-                <button id='echo8'>取得現在地</button>
-            </h5>
-            <div class="form-group">
-                <input type="text" class="form-control" ref="site" v-model="site">
-        
-            </div>
+          <h5>
+            Search：
+            <button id='echo8'>取得現在地</button>
+          </h5>
+          <div class="form-group">
+            <input type="text" class="form-control" ref="site" v-model="site">
+            
+          </div>
         </div>
-    </div>
-    <!-- 放google map的div -->
-    <div class="row">
+      </div>
+      <!-- 放google map的div -->
+      <!-- 用於change event -->
+      <div id="map1"class="row"　style="display:none">
         <div class="col google-map">
-                <h5>Google Map：</h5>
-            <div id="map" class="embed-responsive embed-responsive-16by9"></div>
+            <h5>Google Map：</h5>
+          <div id="map" class="embed-responsive embed-responsive-16by9"></div>
         </div>
-    </div>
-    <hr>
-    <!-- 放評論摘要的div -->
-    <div class="row" v-if="place != null">
+      </div>
+      <hr>
+      <!-- 用於click event -->
+      <div id="map2"class="row">
+        <div class="col google-map">
+            <h5>Google Map：</h5>
+          <div id="map" class="embed-responsive embed-responsive-16by9"></div>
+        </div>
+      </div>
+      <hr>
+      <!-- 放評論摘要的div -->
+      <div class="row" v-if="place != null">
         <div class="col" v-if="place.reviews != null">
-            <h5>評論：</h5>
-            <div class="row" v-for="p in place.reviews">
-                <div class="col">
-                    <ul class="list-unstyled">
-                        <li class="media">
-                            <img :src="p.profile_photo_url" class="mr-3">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">
-                                    <a target="_blank" :href="p.author_url">{{ p.author_name }}</a>
-                                </h5>
-                                <p>{{ p.text }}</p>
-                                <h6>{{ p.relative_time_description }}</h6>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+          <h5>評論：</h5>
+          <div class="row" v-for="p in place.reviews">
+            <div class="col">
+              <ul class="list-unstyled">
+                <li class="media">
+                  <img :src="p.profile_photo_url" class="mr-3">
+                  <div class="media-body">
+                    <h5 class="mt-0 mb-1">
+                      <a target="_blank" :href="p.author_url">{{ p.author_name }}</a>
+                    </h5>
+                    <p>{{ p.text }}</p>
+                    <h6>{{ p.relative_time_description }}</h6>
+                  </div>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
