bf1f5f9cacee1d5d119d0fbab0071c85a2be48c5
bombmanbombman <36650063+bombmanbombman@users.noreply.github.com>
2020-02-01 05:06:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -236,11 +236,11 @@ $(function(){
                     "color":"#ff4000"
                 });
                 if($('a#chinese').hasClass('active')){
-                    $('#error_message').text('歡迎回來 '+array[1]).show(0);
+                    $('#welcome').text('歡迎回來 '+array[1]).fadeToggle().show(0);
                     }else if($('a#japanese').hasClass('active')){
-                        $('#error_message').text('おかえり、'+array[1]+'さん').show(0);
+                        $('#welcome').text('おかえり、'+array[1]+'さん').fadeToggle().show(0);
                     }else if($('a#english').hasClass('active')){
-                        $('#error_message').text('welcome back '+array[1]).show(0);
+                        $('#welcome').text('welcome back '+array[1]).fadeToggle().show(0);
                     }
                 console.log($.cookie());
                 window.location.href='html_userdetail_template.php';
